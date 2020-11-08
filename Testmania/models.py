cc5ed7c9b57a2bcbc7f0784b4b741a85bd010594
Vikas Singh Patel <vkswhy@gmail.com>
2020-11-08 15:52:02 +0530

    nothing special

--- Testmania/models.py
+++ Testmania/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Questions(models.model):
     question=models.CharField(max_length=200)
-    optionA=models.CharField(max_length=100)
-    optionB=models.CharField(_(""), max_length=50)
+    optionA=models.CharField(max_length=50)
+    optionB=models.CharField(max_length=50)
    
 
 # Create your models here.
