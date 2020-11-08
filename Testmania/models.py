cd09e249a755435f2f9fc214e10351ffeb2e9781
nikhil maurya <nikhilmaurya5618056@gmail.com>
2020-11-08 16:00:14 +0530

    fixes

--- Testmania/models.py
+++ Testmania/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-
 class Questions(models.model):
     question=models.CharField(max_length=200)
     optionA=models.CharField(max_length=100)
     optionB=models.CharField(_(""), max_length=50)
-   
-
 # Create your models here.
 class Contests(models.Model){
     contestName=models.CharField(max_length=50)
