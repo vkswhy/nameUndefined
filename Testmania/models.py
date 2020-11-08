58cc36b497f23f3488f51bbc9c688fd6cb1b7351
Vikas Singh Patel <vkswhy@gmail.com>
2020-11-08 16:26:49 +0530

    unchanged

--- Testmania/models.py
+++ Testmania/models.py
@@ -1,2 +1,13 @@
 from django.db import models
 
+
+
+class Contests(models.Model):
+    contestName=models.CharField(max_length=50)
+class Questions(models.Model):
+    question=models.CharField(max_length=200)
+    optionA=models.CharField(max_length=50)
+    optionB=models.CharField(max_length=50)
+    optionC=models.CharField(max_length=50)
+    optionD=models.CharField(max_length=50)
+    contest=models.ForeignKey(Contests,on_delete=models.CASCADE)
