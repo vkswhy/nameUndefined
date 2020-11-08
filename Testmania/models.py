f6a75a21d2defa638d799b616c98e9c70c0ee419
Vikas Singh Patel <vkswhy@gmail.com>
2020-11-08 15:56:58 +0530

    app configured in settings, minor fixes

--- Testmania/models.py
+++ Testmania/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
-class Questions(models.model):
+class Questions(models.Model):
     question=models.CharField(max_length=200)
     optionA=models.CharField(max_length=50)
     optionB=models.CharField(max_length=50)
    
 
 # Create your models here.
-class Contests(models.Model){
+class Contests(models.Model):
     contestName=models.CharField(max_length=50)
-}
+
 
