c3234be98b62c2bcbc8c0dc08880574e2cbfdefa
Vikas Singh Patel <vkswhy@gmail.com>
2020-11-09 16:36:19 +0530

    registration successful

--- Testmania/views.py
+++ Testmania/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from .forms import registerForm
+def homePage(request):
+    return render(request,'base.html')
+def register(request):
+    if request.method=="POST":
+        form=registerForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            user.set_password(user.password)
+            user.save()
+            return HttpResponse(request,"user is valid")
+        else:
+            return render(request,'register.html',{'form':form})
+    else:
+        form=registerForm()
+        return render(request,'register.html',{'form':form})
