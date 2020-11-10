c74beb14471910d8d6e5c933c6ccf788b9c240d1
Vikas Singh Patel <vkswhy@gmail.com>
2020-11-10 11:22:08 +0530

    contests model and questions model implemented

--- Testmania/forms.py
+++ Testmania/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Contests
 from django.contrib.auth.models import User
 
 class registerForm(forms.ModelForm):
@@ -8,3 +9,8 @@ class registerForm(forms.ModelForm):
 class loginForm(forms.Form):
     username=forms.CharField(max_length=15,required=True)
     password=forms.CharField(max_length=15,required=True)
+
+class createTestModelForm(forms.ModelForm):
+    class Meta:
+        model=Contests
+        fields=['contest','noOfQues','startDate','startTime','endDate','endTime','timePerQues']
--- Testmania/models.py
+++ Testmania/models.py
@@ -1,13 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
 class Contests(models.Model):
-    contestName=models.CharField(blank=True,max_length=50)
+    contest=models.CharField(blank=True,max_length=50,default=None)
+    Author=models.OneToOneField(User,on_delete=models.CASCADE,blank=True,default=None)
+    noOfQues=models.IntegerField(blank=True,default=None)
+    startDate=models.DateField(blank=True,default=None)
+    startTime=models.TimeField(blank=True,default=None)
+    endDate=models.DateField(blank=True,default=None)
+    endTime=models.TimeField(blank=True,default=None)
+    timePerQues=models.CharField(blank=True,max_length=12,default=None)
+
+
+
 class Questions(models.Model):
-    question=models.CharField(blank=True,max_length=200)
-    optionA=models.CharField(blank=True,max_length=50)
-    optionB=models.CharField(blank=True,max_length=50)
-    optionC=models.CharField(blank=True,max_length=50)
-    optionD=models.CharField(blank=True,max_length=50)
+    question=models.CharField(blank=True,max_length=200,default=None)
+    optionA=models.CharField(blank=True,max_length=50,default=None)
+    optionB=models.CharField(blank=True,max_length=50,default=None)
+    optionC=models.CharField(blank=True,max_length=50,default=None)
+    optionD=models.CharField(blank=True,max_length=50,default=None)
     contest=models.ForeignKey(Contests,on_delete=models.CASCADE)
+    answer=models.CharField(blank=True,max_length=5,default=None)
+    responseA=models.ManyToManyField(User,related_name="response_a")
+    responseB=models.ManyToManyField(User,related_name="response_b") 
+    responseC=models.ManyToManyField(User,related_name="response_c") 
+    responseD=models.ManyToManyField(User,related_name="response_d") 
+
