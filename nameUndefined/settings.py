4a56aceabfa986bd383d9000ed2ade54c5fab5c4
Vikas Singh Patel <vkswhy@gmail.com>
2020-12-08 10:04:34 +0530

    minor fixes

--- nameUndefined/settings.py
+++ nameUndefined/settings.py
@@ -122,6 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"staticfiles"),]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
